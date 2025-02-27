0139c19514628944f13211b71cedfc564107db75
egeyavuzcan <ege.yavuzcan@hubx.co>
2025-02-27 06:40:28 +0000

    updated_resnet_profile_example

--- resnet/profiler_example.py
+++ resnet/profiler_example.py
@@ -2,21 +2,21 @@ from resnet_model import ResNetModel
 from profiler import JAXProfileManager
 
 def main():
-    # Güncellemeniz gereken yollar:
+    
     image_path = "/home/egeyavuzcan/jax/flaxmodels/flaxmodels/resnet/dimg-18-weighted.png"
-    trace_dir = "/home/egeyavuzcan/jax/flaxmodels/flaxmodels/resnet/profile/jax-trace-resnet"
+    trace_dir = "/home/egeyavuzcan/jax_profile/jaxprofile/resnet"
 
-    # Modeli initialize edelim.
+    
     model = ResNetModel(image_path)
     model.load_image()
     model.initialize_model()
     model.warmup(steps=2)
 
-    # Profil işlemini context manager kullanarak yapıyoruz.
+   
     with JAXProfileManager(trace_dir):
         logits = model.inference()
 
-    print("[main] Profil işlemi tamamlandı. Inference çıktısının şekli:", logits.shape)
+    print("[main] Profile finished", logits.shape)
 
 if __name__ == "__main__":
     main()
